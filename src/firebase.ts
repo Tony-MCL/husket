869bd1fa105d7185ce802234ac3dfa20c256e78c
Tony-MCL <MorningCoffeeLabs@gmail.com>
2026-02-16 20:56:04 +0100

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -8,7 +8,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyBgtEZp3MLwm7QzfZOD355gKMknddE76vk",
   authDomain: "husket-sky.firebaseapp.com",
   projectId: "husket-sky",
-  storageBucket: "husket-sky.firebasestorage.app",
+  storageBucket: "husket-sky.appspot.com",
   messagingSenderId: "384866833179",
   appId: "1:384866833179:web:24e4061fd6a12ae48739be"
 };
